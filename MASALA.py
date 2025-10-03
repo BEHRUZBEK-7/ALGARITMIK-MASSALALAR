f9bef558bb47e70594c8cf36c5bf99c41c7d9515
BEHRUZBEK JUMABOYEV <jumaboyevbehruz148@gmail.com>
2025-10-03 16:28:14 +0500

    masala yechimi

---
+++ MASALA.py
@@ -0,0 +1,51 @@
+#Pyhtonda arifmetik masalalar
+# #1
+# a = int(input("sonni kiriting: "))
+# b = int(input("sonni kiriting: "))
+# sum = a+b
+# print(f"yachim: {sum}")
+# #2
+# a = int(input("ucburchakning uzunligini kiriting: "))
+# b = int(input("uchburchakning enini kiriting: "))
+# area = a * b
+# print(f"yuzi: {area}")
+#3
+# a = int(input("birinchi sonni kiriting: "))
+# b = int(input("ikkinchi sonni kiriting: "))
+# c = int(input("uchinchi sonni kiriting: "))
+# avarege  = (a + b + c) / 3
+# print(f"ortacha arifimetik sonni: {avarege}")
+#4
+# a = int(input("sonni kiriting"))
+# b = int(input("sonni kiriting: "))
+# p = 2 * (a + b)
+# print(f"tortburchak perimetri: {p}")
+#5
+# h = int(input("soatni kiriting: "))
+# m = int(input("daqiqani kiriting: "))
+# total_min=h * 60 + m
+# print(f"yechim: {total_min}")
+#6
+# usd = int(input("pulni kiriting: "))
+# sum = usd * 12000
+# print(f"pul: {sum}")
+#7
+# a = int(input("sonni kiriting: "))
+# b = int(input("sonni kiriting: "))
+# diff = abs(a - b)
+# print(f"yechim: {diff}")
+#8
+# x = int(input("sonni kiriting: "))
+# n = int(input("sonni kiriting: "))
+# power = x ** n
+# print(f"yechim: {power}")
+#9
+# a = int(input("sonni kiriting: "))
+# b = int(input("sonni kiriting: "))
+# quotient = a // b
+# print(f"yechim: {quotient}")
+#10
+# a = int(input("sonni kiriting: "))
+# b = int(input("sonni kiriting: "))
+# a, b = b, a
+# print(f"yechim: {a, b}")
